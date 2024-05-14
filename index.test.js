1880bb53067eb849e2d09d0f48e7d988d77d1212
João Oliveira <jaoovit@gmail.com>
2024-05-14 15:31:26 +0100

    create a reverse string function

--- index.test.js
+++ index.test.js
@@ -1,7 +1,13 @@
-const capitalize = require("./index.js");
+const { capitalize, reverseString } = require("./index.js");
 
 describe("capitalize function", () => {
   test("the output is a capitalized word", () => {
     expect(capitalize("test")).toBe("Test");
+  });
+});
+
+describe("reveseString function", () => {
+  test("the output is a revesed string", () => {
+    expect(reverseString("string")).toBe("gnirts");
   });
 });
